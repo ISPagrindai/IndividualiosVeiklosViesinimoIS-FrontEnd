f9eef36b7ddcf24214ceb3b25243550870589c28
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-11-07 23:38:04 +0200

    Basic home page design

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,14 +1,57 @@
+import {Button} from 'react-bootstrap';
 
 export default function Home(){
     return(
         <>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm" style={{"background-color": "#d6f2c2", height: "100vh"}}>
-                        <p>kazkas</p>
+                    <div className="col-sm d-flex justify-content-center" style={{"background-color": "#d6f2c2", height: "100vh"}}>
+                        <div className="align-self-center">
+                            <div>
+                                <h3>Ieškai papildomo darbo?</h3>
+                            </div>
+                            <div>
+                                IMG
+                            </div>
+                            <div>
+                                <p>Išsirink patinkantį darbą sau tinkamu metu ir kandidatuok.</p>
+                            </div>
+                            <div>
+                                <Button size="lg" variant="secondary">Ieškau darbo</Button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-sm" style={{"background-color": "#b9e4f4", height: "100vh"}}>
-                        <p>kazkas</p>
+                    <div className="col-sm d-flex justify-content-center" style={{"background-color": "#b9e4f4", height: "100vh"}}>
+                        <div className="align-self-center">
+                            <div>
+                                <h3>Reikia darbuotojų skubiai?</h3>
+                            </div>
+                            <div>
+                                IMG
+                            </div>
+                            <div>
+                                <p>Siūlome naują ir modernų būdą surasti darbuotojus trumpalaikiams darbams.</p>
+                            </div>
+                            <div>
+                                <Button size="lg" variant="secondary">Reikia darbuotojų</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm d-flex justify-content-center" style={{"background-color": "#9DD9D2", height: "100vh"}}>
+                        <div className="align-self-center">
+                            <div>
+                                <h3>Esi savo srities specialistas?</h3>
+                            </div>
+                            <div>
+                                IMG
+                            </div>
+                            <div>
+                                <p>Įkelk savo sugebėjimus ir teikiamas paslaugas ir būk pastebėtas</p>
+                            </div>
+                            <div>
+                                <Button size="lg" variant="secondary">Teikiu paslaugas</Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
