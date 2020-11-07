b462c0b2e9df33fb41c468efc7fc58542cdc9011
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-11-07 21:08:28 +0200

    Added login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,47 @@
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Modal, Form } from 'react-bootstrap'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import LoginService from '../Services/LoginService'
 
 export default function Login(){
+    const [show, setShow] = useState(false);
+    const { register, handleSubmit, watch, errors } = useForm();
 
-    const onClickHandler = () =>{
-        console.log("Pavyko");
+    const onSubmit = (data) => {
+        setShow(false);
+        //TODO: call login service
     }
+
     return (
-        <Button variant="secondary" onClick={onClickHandler}>Prisijungti</Button>
+        <>
+        <Button variant="secondary" onClick={() => setShow(true)}>Prisijungti</Button>
+        <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Prisijungti
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form onSubmit={handleSubmit(onSubmit)} id="LoginForm">
+            <Form.Group>
+                <Form.Label>El. Paštas</Form.Label>
+                <Form.Control type="email" name="email" ref={register} placeholder="el.paštas"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Slaptažodis</Form.Label>
+                <Form.Control type="password" name="password" ref={register} placeholder="slaptažodis"/>
+            </Form.Group>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button type="submit" form="LoginForm" variant="secondary">Prisijungti</Button>
+        </Modal.Footer>
+      </Modal>
+        </>
     );
 }
