d0e73baedad617ec4c432922ad8f6eaaa10efe5f
Aivaras <aivariuxxxl@gmail.com>
2020-11-09 14:03:26 +0300

    individual work card list

--- src/Views/IndividualWorkView.js
+++ src/Views/IndividualWorkView.js
@@ -5,6 +5,7 @@ import styles from "../IndividualWork.module.css";
 import classNames from "classnames";
 import { useState } from "react";
 import Modal from 'react-bootstrap/Modal'
+import '../individualWorkCard.css'
 
 export default function IndividualWorkView() {
   const [show, setShow] = useState(false);
@@ -14,78 +15,130 @@ export default function IndividualWorkView() {
 
   return (
     <>
-      <div className="container-fluid">
+      <div className="container my-5">
         <div className="row">
-          <div
-            className="col-sm-4 d-flex justify-content-center"
-            style={{
-              height: "75vh",
-              "margin-top": "auto",
-              "margin-bottom": "auto",
-            }}
-          >
-            <Image
-              style={{ "max-width": "100%" }}
-              src={`${process.env.PUBLIC_URL}/assets/images/photo.jpg`}
-              style={{ border: "5px solid #576675", padding: 0 }}
-              fluid
-            />
-          </div>
-          <div
-            className="col-sm-8 d-flex justify-content-center"
-            style={{
-              "background-color": "#faebd7",
-              border: "5px solid #576675",
-              padding: 0,
-              height: "100vh",
-            }}
-          >
-            <div>
-              <div className="mt-5 mr-5 text-right">
-                <Button href="vipForm" variant="info">
-                  Iškelti į priekį!
-                </Button>{" "}
-              </div>
-              <div className={styles.mtcustom}>
-                <h2>-- Muzikantė - Gitaristė --</h2>
-              </div>
-              <div className={styles.mtcustom}>
-                <h4>
-                  <a href="userProfile" style={{ color: "#000" }}>
-                    Ingrida Ramanauskaitė
-                  </a>
-                </h4>
-              </div>
-              <div className={classNames(styles.mtcustom, styles.mxcustom)}>
-                <p>
-                  Nuo pat mažumos mane traukia muzika, 7 metu jau mokėjau groti
-                  penkiais instrumentais, tačiau, pagal mane, niekas niekad
-                  neaplenks gitaros. 2016 metais laimėjau Lietuvos balsą bei
-                  turiu daugelį apdovanojimų iš muzikos konkursų. Tad jeigu
-                  kažką domina talentinga muzikante, ar tai būtų vestuvėms,
-                  gimtadieniui ar šiaip mini-koncertui, susisiekite su manim.
-                </p>
-              </div>
-              <div className={classNames("row", styles.mtcustom)}>
-                <div className="col">Telefonas: 868405446</div>
-                <div className="col">
-                  El. Paštas: Ingrida.Gitaristė@muzika.lt
+          <div className="col-sm-12 col-md-8 col-lg-6">
+            <div className="card my-5">
+              <h5 className="card-header">-- Muzikantė - Gitaristė --</h5>
+              <div className="row">
+                <div className="col-6 container-profile">
+                  <img className="card-img"
+                  src={`${process.env.PUBLIC_URL}/assets/images/photo.jpg`}
+                  ></img>
+                  <div className="overlay individualWorkCard">
+                    <Button className="text" href="userProfile"  variant="success">
+                    Peržiūrėti profilį
+                    </Button>{" "}
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="card-title">
+                    <a href="userProfile" style={{ color: "#000" }}></a>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">
+                      <Button href="review" variant="info">
+                        Iškelti profilį !
+                      </Button>{" "}
+                      </li>
+                      <li className="list-group-item"
+                          style={{ color: "#000" }}
+                          >
+                        Ingrida Ramanauskaitė
+                      </li>
+                      <li className="list-group-item">
+                        Telefonas: 868405446
+                      </li>
+                      <li className="list-group-item">
+                        El. Paštas: Ingrida.Gitaristė@muzika.lt
+                      </li>
+                      <br/>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="card-text" style={{ height: 150 }}>
+                    Nuo pat mažumos mane traukia muzika, 7 metu jau mokėjau groti
+                    penkiais instrumentais, tačiau, pagal mane, niekas niekad
+                    neaplenks gitaros. 2016 metais laimėjau Lietuvos balsą bei
+                    turiu daugelį apdovanojimų iš muzikos konkursų. Tad jeigu
+                    kažką domina talentinga muzikante, ar tai būtų vestuvėms,
+                    gimtadieniui ar šiaip mini-koncertui, susisiekite su manim.
+                  </div>
                 </div>
               </div>
-              <div className={classNames("btn-group", styles.mtcustom)}>
+              <div className="card-footer col-12">
+                <Button href="review" variant="success">
+                  Palikti atsiliepimą
+                </Button>{" "}
                 <Button
                   href="individualWorkEdit"
                   className={styles.btnspace}
                   variant="warning"
                 >
-                  Redaguoti
+                Redaguoti
                 </Button>{" "}
                 <Button onClick={handleShow} variant="danger">Ištrinti</Button>{" "}
               </div>
-              <div className={styles.mtcustom}>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-8 col-lg-6">
+            <div className="card my-5">
+              <h5 className="card-header">-- Sodininkas --</h5>
+              <div className="row">
+                <div className="col-6 container-profile">
+                  <img className="card-img"
+                  src={`${process.env.PUBLIC_URL}/assets/images/gardener.jpg`}
+                  ></img>
+                  <div className="overlay individualWorkCard">
+                    <Button className="text" href="userProfile"  variant="success">
+                    Peržiūrėti profilį
+                    </Button>{" "}
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="card-title">
+                    <a href="userProfile" style={{ color: "#000" }}></a>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">
+                      <Button href="review" variant="info">
+                        Iškelti profilį !
+                      </Button>{" "}
+                      </li>
+                      <li className="list-group-item"
+                          style={{ color: "#000" }}
+                          >
+                        Tomas Žaliauskas
+                      </li>
+                      <li className="list-group-item">
+                        Telefonas: 865498612
+                      </li>
+                      <li className="list-group-item">
+                        El. Paštas: tomas.sodas@gmail.com
+                      </li>
+                      <br/>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="card-text" style={{ height: 150 }}>
+                    Man labai patinka gamta, todėl kasdien dirbu lauke.
+                    Galiu griebti lapus, genėti medžius ir praktiškai visą kitą,
+                    kas susiję su sodininkyste.
+                  </div>
+                </div>
+              </div>
+              <div className="card-footer col-12">
                 <Button href="review" variant="success">
                   Palikti atsiliepimą
                 </Button>{" "}
+                <Button
+                  href="individualWorkEdit"
+                  className={styles.btnspace}
+                  variant="warning"
+                >
+                Redaguoti
+                </Button>{" "}
+                <Button onClick={handleShow} variant="danger">Ištrinti</Button>{" "}
               </div>
             </div>
           </div>
