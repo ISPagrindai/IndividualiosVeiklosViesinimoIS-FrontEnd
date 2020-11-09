d6c980d3dedd239ce5739df27f580dc7b1ad62c5
Aiste Juozapaviciute <aiste.juozapaviciute@gmail.com>
2020-11-09 21:57:04 +0200

    fixed bug

--- src/Views/AdminView.js
+++ src/Views/AdminView.js
@@ -1,28 +1,40 @@
-import { Button } from 'react-bootstrap';
-
+import { Button } from "react-bootstrap";
 
 export default function AdminView() {
-    return (
-        <>
-            <div>
-                <Button href="adminForm" style={{float: "right"}}>Kurti naują administratorių</Button>
-            </div>
-                <br></br>
-            <div className="row">
-                <div className="col-4">
-                    <Button className="mb-3" size="lg" href="individualWork" >Peržiūrėti individualių darbų sąrašą</Button>
-                    <Button size="lg" href="jobs">Peržiūrėti trumpalaikių darbų sąrašą</Button>
-                </div>
-                <div className="col-4">
-                    <Button className="mb-3" size="lg" href="employerAdmin" >Peržiūrėti įmonių sąrašą</Button>    
-                    <Button size="lg" href="newEmployer" >Peržiūrėti naujų įmonių sąrašą</Button>
-                </div>
-                <div className="col-4">
-                    <Button className="mb-3" size="lg" href="user" >Peržiūrėti klientų paskyrų sąrašą</Button>
-                    <Button size="lg" href="reports" >Peržiūrėti ataskaitas</Button>
-                </div>
-
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <Button href="adminForm" style={{ float: "right" }}>
+          Kurti naują administratorių
+        </Button>
+      </div>
+      <br></br>
+      <div className="row">
+        <div className="col-4">
+          <Button className="mb-3" size="lg" href="individualWorkList">
+            Peržiūrėti individualių darbų sąrašą
+          </Button>
+          <Button size="lg" href="jobs">
+            Peržiūrėti trumpalaikių darbų sąrašą
+          </Button>
+        </div>
+        <div className="col-4">
+          <Button className="mb-3" size="lg" href="employerAdmin">
+            Peržiūrėti įmonių sąrašą
+          </Button>
+          <Button size="lg" href="newEmployer">
+            Peržiūrėti naujų įmonių sąrašą
+          </Button>
+        </div>
+        <div className="col-4">
+          <Button className="mb-3" size="lg" href="user">
+            Peržiūrėti klientų paskyrų sąrašą
+          </Button>
+          <Button size="lg" href="reports">
+            Peržiūrėti ataskaitas
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
