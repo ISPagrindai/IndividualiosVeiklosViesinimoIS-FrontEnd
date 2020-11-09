fd540a71267512d57cd66b5b6def4ed3cf016b4c
Aiste Juozapaviciute <aiste.juozapaviciute@gmail.com>
2020-11-09 22:40:35 +0200

    fixed routes to job and user pages

--- src/Components/AdminComponents/Employer.js
+++ src/Components/AdminComponents/Employer.js
@@ -1,18 +1,29 @@
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 
-export default function Employer(props){
-    return(
-        <tr>
-            <td><a href={`employer/${props.data.id}`}>{props.data.name}</a></td>
-            <td>{props.data.description}</td>
-            <td>{props.data.city}</td>
-            <td>{props.data.address}</td>
-            {props.employer ? 
-            <td>
-                <Button variant="secondary" href={`employer/edit/${props.data.id}`}>Redaguoti</Button>
-                <Button variant="danger" onClick={props.show}>Trinti</Button>
-                <Button variant="danger" onClick={props.show}>Užšaldyti</Button>
-            </td> : ""}
-        </tr>
-    );
+export default function Employer(props) {
+  return (
+    <tr>
+      <td>
+        <a href={`employer/${props.data.id}`}>{props.data.name}</a>
+      </td>
+      <td>{props.data.description}</td>
+      <td>{props.data.city}</td>
+      <td>{props.data.address}</td>
+      {props.employer ? (
+        <td>
+          <Button variant="secondary" href={`job/edit/${props.data.id}`}>
+            Redaguoti
+          </Button>
+          <Button variant="danger" onClick={props.show}>
+            Trinti
+          </Button>
+          <Button variant="danger" onClick={props.show}>
+            Užšaldyti
+          </Button>
+        </td>
+      ) : (
+        ""
+      )}
+    </tr>
+  );
 }
--- src/Components/AdminComponents/User.js
+++ src/Components/AdminComponents/User.js
@@ -1,19 +1,28 @@
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 
-export default function User(props){
-    return(
-        <tr>
-            <td><a href={`user/${props.data.id}`}>{props.data.name}</a></td>
-            <td>{props.data.surname}</td>
-            <td>{props.data.birthday}</td>
-            <td>{props.data.sex}</td>
-            <td>{props.data.phone}</td>
-            <td>{props.data.email}</td>
-            {props.flag ? 
-            <td>
-                <Button variant="secondary" href={`user/edit/${props.data.id}`}>Redaguoti</Button>
-                <Button variant="danger" onClick={props.show}>Užšaldyti</Button>               
-            </td> : ""}
-        </tr>
-    );
+export default function User(props) {
+  return (
+    <tr>
+      <td>
+        <a href={`user/${props.data.id}`}>{props.data.name}</a>
+      </td>
+      <td>{props.data.surname}</td>
+      <td>{props.data.birthday}</td>
+      <td>{props.data.sex}</td>
+      <td>{props.data.phone}</td>
+      <td>{props.data.email}</td>
+      {props.flag ? (
+        <td>
+          <Button variant="secondary" href="individualWorkEdit">
+            Redaguoti
+          </Button>
+          <Button variant="danger" onClick={props.show}>
+            Užšaldyti
+          </Button>
+        </td>
+      ) : (
+        ""
+      )}
+    </tr>
+  );
 }
