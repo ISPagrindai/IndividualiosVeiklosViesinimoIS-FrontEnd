f0db1fd2b8e576698d17d63641bba423a2fc83e1
Aivaras <aivariuxxxl@gmail.com>
2020-11-09 20:57:43 +0300

    Fixed individualWorkList and added individualWork view

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './Components/Navigation'
 import EmployeesView from './Views/EmployeesView';
 import EmployersView from './Views/EmployersView';
 import IndividualWorkView from './Views/IndividualWorkView';
+import IndividualWorkListView from './Views/IndividualWorkListView';
 import AdminView from './Views/AdminView';
 import Home from './Views/Home';
 import Footer from './Components/Footer';
@@ -18,6 +19,7 @@ import IndividualWorkForm from './Views/IndividualWorkForm';
 import CandidatesView from './Views/CandidatesView'
 import JobHistory from './Views/JobHistory';
 import Applicant from './Views/Applicant';
+import EmployeeOrderView from './Views/EmployeeOrderView';
 
 function App() {
   return (
@@ -28,6 +30,9 @@ function App() {
         <Switch>
           <Route exact strict path="/">
             <Home />
+          </Route>
+          <Route path="/individualWorkList">
+            <IndividualWorkListView />
           </Route>
           <Route path="/individualWork">
             <IndividualWorkView />
@@ -73,6 +78,9 @@ function App() {
           </Route>
           <Route path="/applicant">
             <Applicant />
+          </Route>
+          <Route path="/employeeOrder">
+            <EmployeeOrderView />
           </Route>
         </Switch>
         </div>
