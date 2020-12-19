b0f8338b73fc29956a4ffc801d6b0dd64fe6a1c7
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-19 18:10:58 +0200

    url fix

--- src/Services/restApiService.js
+++ src/Services/restApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://localhost:5001';
+const API_URL = 'https://localhost:5001/api';
 
 export default function setAuthorizationToken(token) {
   axios.defaults.headers.common['Authorization'] = token;
