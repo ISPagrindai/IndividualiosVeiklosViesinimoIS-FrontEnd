d74d2c49d5a521d0dae65b0fa6df86ac2f335512
RobertasPetrauskas <robertas.petrauskas0@gmail.com>
2020-12-20 23:32:35 +0200

    User register

--- src/Services/LoginService.js
+++ src/Services/LoginService.js
@@ -1,4 +1,5 @@
-import {get, post, setAuthorizationToken} from './restApiService'
+import {errorToString, get, post, setAuthorizationToken} from './restApiService'
+import NotificationService from './NotificationService'
 import cookie from 'js-cookie';
 
 export function Login(login){
@@ -12,6 +13,24 @@ export function Login(login){
             return false;
         })
         .catch(error => console.log(error));
+}
+export function RegisterUserService(data){
+    return post('/Users/registerCompany', data)
+        .then(() => {
+            NotificationService.success("Sėkmingai užsiregistravote, dabar prisijunkite")
+        })
+        .catch(error => {
+            NotificationService.error(errorToString(error));
+        });
+}
+export function RegisterCompanyService(data){
+    return post('/Users/register', data)
+        .then(() => {
+            NotificationService.success("Sėkmingai užsiregistravote, dabar prisijunkite")
+        })
+        .catch(error => {
+            NotificationService.error(errorToString(error));
+        });
 }
 export function VerifyToken(token){
     return get('/Verify')
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'react-notifications-component/dist/theme.css'
+import ReactNotification from 'react-notifications-component';
 import App from './App';
 
 ReactDOM.render(
   <>
+  <ReactNotification />
     <App />
   </>,
   document.getElementById('root')
